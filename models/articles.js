9d80887fc68fefa5ea5fc6cd1d96db65327d1349
SamR232 <samrichmond0203@hotmail.co.uk>
2022-03-29 14:14:02 +0100

    404 added to articles

--- models/articles.js
+++ models/articles.js
@@ -7,6 +7,9 @@ exports.selectArticleById = (articleId) => {
   return db
     .query(`SELECT * FROM articles WHERE article_id = $1;`, [articleId])
     .then((result) => {
+      if (result.rows.length === 0) {
+        return Promise.reject({ status: 404, msg: "article not found" });
+      }
       return result.rows[0];
     });
 };
