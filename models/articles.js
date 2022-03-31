f9c47706ce671f11bf71f86cdc292d7bff7f6812
SamR232 <samrichmond0203@hotmail.co.uk>
2022-03-31 11:48:52 +0100

    updated to complete all inside sql query

--- models/articles.js
+++ models/articles.js
@@ -3,25 +3,19 @@ const db = require("../db/connection");
 exports.selectArticleById = (articleId) => {
   return db
     .query(
-      `SELECT * FROM articles 
-    WHERE article_id = $1;`,
+      `SELECT articles.*, COUNT(comments.article_id) :: INT AS comment_count
+      FROM articles
+      LEFT JOIN comments 
+      ON articles.article_id = comments.article_id
+      WHERE articles.article_id = $1
+      GROUP BY articles.article_id;`,
       [articleId]
     )
     .then((articleInfo) => {
       if (articleInfo.rows.length === 0) {
         return Promise.reject({ status: 404, msg: "article not found" });
       } else {
-        return db
-          .query(
-            "SELECT COUNT(comment_id) FROM comments WHERE article_id = $1",
-            [articleId]
-          )
-          .then((commentCount) => {
-            articleInfo.rows[0]["comment_count"] = Number(
-              commentCount.rows[0].count
-            );
-            return articleInfo.rows[0];
-          });
+        return articleInfo.rows[0];
       }
     });
 };
