64ca59101e5cfd66c8a50a3f5d57f365f070205a
SamR232 <samrichmond0203@hotmail.co.uk>
2022-03-31 10:31:06 +0100

    get comment count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -146,12 +146,12 @@ describe("GET /api/articles/:article_id (comment count)", () => {
         expect(typeof articleInfo["comment_count"]).toBe("number");
       });
   });
-  //   test("Status: 404 - article id does not exist", () => {
-  //     return request(app)
-  //       .get("/api/articles/1")
-  //       .expect(200)
-  //       .then(({ body }) => {
-  //         expect(body.msg).toEqual("article not found");
-  //       });
-  //   });
+  test("Status: 404 - article id does not exist", () => {
+    return request(app)
+      .get("/api/articles/1000")
+      .expect(404)
+      .then(({ body }) => {
+        expect(body.msg).toEqual("article not found");
+      });
+  });
 });
