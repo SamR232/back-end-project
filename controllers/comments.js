1134b15f4df7fb0d9e375760b517c29c16b06bc5
SamR232 <samrichmond0203@hotmail.co.uk>
2022-04-01 10:52:34 +0100

    tests added

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,8 +1,12 @@
 const { selectArticleComments } = require("../models/comments");
 
-exports.getArticleComments = (req, res) => {
+exports.getArticleComments = (req, res, next) => {
   let { article_id } = req.params;
+
   selectArticleComments(article_id).then((comments) => {
     res.status(200).send(comments);
-  });
+  })
+  .catch((err)=> {
+    next(err)
+  })
 };
