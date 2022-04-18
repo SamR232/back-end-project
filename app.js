0c040f4ef27ce5181f22e508d9992ba4ac9b91c5
SamR232 <samrichmond0203@hotmail.co.uk>
2022-04-18 15:02:17 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
 //Require in topics
 const { getTopics } = require("./controllers/topics");
